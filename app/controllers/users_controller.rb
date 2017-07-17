340936bd9f2b2897be6af6515f13ebb4432ae0a0
take79 <tk.hrsk79@gmail.com>
2017-07-17 16:22:15 +0900

    ignore swp, add user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,88 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  # GET /data
+  # GET /data.json
+  def index
+    @Users = user.all
+  end
+
+  # GET /data/1
+  # GET /data/1.json
+  def show
+  end
+
+  # GET /data/new
+  def new
+    @datum = Datum.new
+    @datum.build_gesture_tag
+  end
+
+  # GET /data/1/edit
+  def edit
+  end
+
+  # POST /data
+  # POST /data.json
+  def create
+    @datum = Datum.new(datum_params)
+
+    respond_to do |format|
+      if @datum.save
+        format.html { redirect_to @datum, notice: 'Datum was successfully created.' }
+        format.json { render :show, status: :created, location: @datum }
+      else
+        format.html { render :new }
+        format.json { render json: @datum.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /data/1
+  # PATCH/PUT /data/1.json
+  def update
+    respond_to do |format|
+      if @datum.update(datum_params)
+        format.html { redirect_to @datum, notice: 'Datum was successfully updated.' }
+        format.json { render :show, status: :ok, location: @datum }
+      else
+        format.html { render :edit }
+        format.json { render json: @datum.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /data/1
+  # DELETE /data/1.json
+  def destroy
+    @datum.destroy
+    respond_to do |format|
+      format.html { redirect_to data_url, notice: 'Datum was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  def fetch_gesture
+    filepath = @datum.gesture.path
+    stat = File::stat(filepath)
+    send_file(filepath, :filename => 'gesture.bvh', :length => stat.size)
+  end
+
+  def fetch_audio
+    filepath = @datum.audio.path
+    stat = File::stat(filepath)
+    send_file(filepath, :filename => 'audio.bvh', :length => stat.size)
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_datum
+      @datum = Datum.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def datum_params
+      params.require(:datum).permit(:name, :audio, :video, :gesture, :actor_id, :topic_id, :gesture_tag_id, :keywords, gesture_tag_attributes: [:iconic, :metaphoric, :deictic, :beat])
+    end
+end
+end
