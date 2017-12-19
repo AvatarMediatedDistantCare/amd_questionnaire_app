c2d435e7c927c83378a86a246c539cf8466a2d4d
Kengo Obana <aoyama.banaoh@gmail.com>
2017-12-19 14:57:22 +0900

    counter_balanceを意識したランダマイズの実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -34,122 +34,53 @@ class SessionsController < ApplicationController
       [2 ,1, 1]
     ]
 
-    motion_id_list = (0..7).to_a
+    motion_id_list = (0..23).to_a
 
-    motion_id_list.each do |m_id|
-      for gesture_type in 0..2 do
-        @ans = @session.answers.build
-        @ans.motion_id = m_id
-        @ans.gesture_type = gesture_type
-        @ans.save
+    motion_order_list = MotionOrder.where(applying_user_id: @session.user_id)
+    motion_order_list.each_with_index do |motion, idx|
+      if( motion.order_type == 0 )
+        @kinect_ans = @session.answers.build
+        @kinect_ans.motion_id = idx
+        @kinect_ans.gesture_type = 0
+        @kinect_ans.save
+
+        @proposed_ans = @session.answers.build
+        @proposed_ans.motion_id = idx
+        @proposed_ans.gesture_type = 1
+        @proposed_ans.save
+      else
+        @proposed_ans = @session.answers.build
+        @proposed_ans.motion_id = idx
+        @proposed_ans.gesture_type = 1
+        @proposed_ans.save
+
+        @kinect_ans = @session.answers.build
+        @kinect_ans.motion_id = idx
+        @kinect_ans.gesture_type = 0
+        @kinect_ans.save
       end
     end
     
     avator_answers_list = @session.answers
 
-
-    motion_id_list.each do |m_id|
-      user_order = @session.user_id%6
-      motion_order = order_list[ MotionOrder.find_by(motion_id: m_id, applying_user_type: user_order).order_type ]
-      if m_id == 0
-        motion_order.each_with_index do |m_order, index|
-          @ans = @session.answers.find_by(motion_id: m_id, gesture_type: m_order[index])
-          if index == 0
-            @ans.prev_id = nil
-            @ans.next_id = @session.answers.find_by(motion_id: m_id, gesture_type: m_order[index+1]).id
-          elsif index == 2
-            @ans.prev_id = @session.answers.find_by(motion_id: m_id, gesture_type: m_order[index-1]).id
-            # binding.pry
-            next_first_motion_type = order_list[ MotionOrder.find_by(motion_id: m_id+1, applying_user_type: user_order).order_type ].first
-            @ans.next_id = @session.answers.find_by(motion_id: m_id + 1, gesture_type: next_first_motion_type).id
-          else
-            @ans.prev_id = @session.answers.find_by(motion_id: m_id, gesture_type: m_order[index-1])
-            @ans.next_id = @session.answers.find_by(motion_id: m_id, gesture_type: m_order[index+1])
-          end
-          @ans.order = index+1
-          @ans.save
-        end
-      elsif m_id == 7
-        motion_order.each_with_index do |m_order, index|
-          @ans = @session.answers.find_by(motion_id: m_id, gesture_type: m_order[index])
-          if index == 0
-            prev_last_motion_type = order_list[ MotionOrder.find_by(motion_id: m_id-1, applying_user_type: user_order).order_type ].last
-            @ans.prev_id = @session.answers.find_by(motion_id: m_id - 1, gesture_type: prev_last_motion_type).id
-            @ans.next_id = @session.answers.find_by(motion_id: m_id, gesture_type: m_order[index+1]).id
-          elsif index == 2
-            @ans.prev_id = @session.answers.find_by(motion_id: m_id, gesture_type: m_order[index-1]).id
-            @ans.next_id = nil            
-          else
-            @ans.prev_id = @session.answers.find_by(motion_id: m_id, gesture_type: m_order[index-1])
-            @ans.next_id = @session.answers.find_by(motion_id: m_id, gesture_type: m_order[index+1])
-          end
-          @ans.order = index+1
-          @ans.save
-        end
+    avator_answers_list.each_with_index do |ans, idx|
+      @ans = ans
+      if idx == 0
+        prev_id = nil
+        next_id = avator_answers_list.find_by(motion_id: ans.motion_id).id
+      elsif idx == avator_answers_list.length - 1
+        prev_id = avator_answers_list[idx-1].id
+        next_id = nil
       else
-        motion_order.each_with_index do |m_order, index|
-          @ans = @session.answers.find_by(motion_id: m_id, gesture_type: m_order[index])
-          if index == 0
-            prev_last_motion_type = order_list[ MotionOrder.find_by(motion_id: m_id-1, applying_user_type: user_order).order_type ].last
-            @ans.prev_id = @session.answers.find_by(motion_id: m_id - 1, gesture_type: prev_last_motion_type).id
-            @ans.next_id = @session.answers.find_by(motion_id: m_id, gesture_type: m_order[index+1]).id
-          elsif index == 2
-            @ans.prev_id = @session.answers.find_by(motion_id: m_id, gesture_type: m_order[index-1]).id
-            next_first_motion_type = order_list[ MotionOrder.find_by(motion_id: m_id+1, applying_user_type: user_order).order_type ].first
-            @ans.next_id = @session.answers.find_by(motion_id: m_id + 1, gesture_type: next_first_motion_type).id
-          else
-            @ans.prev_id = @session.answers.find_by(motion_id: m_id, gesture_type: m_order[index-1])
-            @ans.next_id = @session.answers.find_by(motion_id: m_id, gesture_type: m_order[index+1])
-          end
-          @ans.order = index+1
-          @ans.save
-        end
+        prev_id = avator_answers_list[idx-1].id
+        next_id = avator_answers_list[idx+1].id
       end
+      @ans.order = idx + 1
+      @ans.prev_id = prev_id
+      @ans.next_id = next_id
+      @ans.save
     end
-    
 
-    # avator_answers_list.each_with_index do |ans, idx|
-    #   @ans = ans
-    #   if idx == 0
-    #     prev_id = nil
-    #     next_id = avator_answers_list.where(motion_id: ans.m_id).id
-    #   elsif idx == avator_answers_list.length - 1
-    #     prev_id = avator_answers_list[idx-1].id
-    #     next_id = nil
-    #   else
-    #     prev_id = avator_answers_list[idx-1].id
-    #     next_id = avator_answers_list[idx+1].id
-    #   end
-    #   @ans.order = idx + 1
-    #   @ans.prev_id = prev_id
-    #   @ans.next_id = next_id
-    #   @ans.save
-    # end
-
-    # skeleton_answers_list.each_with_index do |ans, idx|
-    #   @ans = ans
-    #   if idx == 0
-    #     prev_id = nil
-    #     next_id = skeleton_answers_list[idx+1].id
-    #   elsif idx == skeleton_answers_list.length - 1
-    #     prev_id = skeleton_answers_list[idx-1].id
-    #     next_id = nil
-    #   else
-    #     prev_id = skeleton_answers_list[idx-1].id
-    #     next_id = skeleton_answers_list[idx+1].id
-    #   end
-    #   @ans.order = idx + 1
-    #   @ans.prev_id = prev_id
-    #   @ans.next_id = next_id
-    #   @ans.save
-    # end
-
-    # @avator_ans = @session.answers.where(avator_type: 0).find_by(next_id: nil)
-    # @skeleton_ans = @session.answers.where(avator_type: 1).find_by(prev_id: nil)
-    # @avator_ans.next_id = @skeleton_ans.id
-    # @skeleton_ans.prev_id = @avator_ans.id
-    # @avator_ans.save
-    # @skeleton_ans.save
 
     respond_to do |format|
       if @session.save
