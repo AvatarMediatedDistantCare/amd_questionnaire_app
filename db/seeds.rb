f38ac83c116ee5105b51e0cb7f25cb782ccec670
Kengo Obana <aoyama.banaoh@gmail.com>
2017-12-20 16:44:17 +0900

    seedの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,15 @@
 # motionの数
 motion_id = (0..7).to_a # motion_index
-applying_user_id = (1...120).to_a # amount of users
+applying_user_group = (0...30).to_a # amount of users
 counter_balance = (0..5).to_a.shuffle # 提示順序の組み合わせ
+user_index = (1..6).to_a
 
-
-motion_id.each do |m_id|
-  applying_user_id.each do |u_id|
-    order_type = counter_balance.pop
-    if(order_type == nil)
-      counter_balance = (0..2).to_a.shuffle
-      order_type = counter_balance.pop
+applying_user_group.each do |group_id|
+  motion_id.each do |m_id|
+    counter_balance = (0..5).to_a.shuffle # 提示順序の組み合わせ
+    user_index.each do |u_idx|
+      MotionOrder.create( motion_id: m_id, order_type: counter_balance.pop, applying_user_id: u_idx + group_id*6 )
     end
-    MotionOrder.create( motion_id: m_id, order_type: order_type, applying_user_id: u_id )
   end
 end
+
